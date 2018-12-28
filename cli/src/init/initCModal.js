58ef49bc07fb6f731c324ba868e2eb69df12c8aa
gongjinhua <215822834@qq.com>
2018-12-28 14:54:09 +0800

    Modal

---
+++ cli/src/init/initCModal.js
@@ -0,0 +1,12 @@
+/**
+ * 初始化CURD modal
+ */
+const vtxUtil = require('../util/vtxUtil.js');
+const split_array = vtxUtil.split_array;
+
+function initCModal(parameters) {
+
+	
+}
+
+module.exports = initCModal;
